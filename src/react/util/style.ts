9d4fadec8f3d9d04b455c68621ed9a214336df12
slightlycyborg <collinalexbell@gmail.com>
2020-02-09 20:49:09 +0000

    rm util/style.ts

--- src/react/util/style.ts
+++
@@ -1,9 +0,0 @@
-
-export const css = (cssObj: {[key: string]: string}): string => {
-  let str = '';
-  for(const item in cssObj) {
-    str += `${item}: ${cssObj[item]};`;
-  }
-  return str;
-};
-
